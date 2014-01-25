95f73c38363c5a8246f9c8ec83a3bd89a1ddccf4
Will <william.g.armstrong@gmail.com>
2014-01-25 14:09:58 -0500

    flip em

--- App/app/src/main/java/com/rambutan/digitalflyers/MainActivity.java
+++ App/app/src/main/java/com/rambutan/digitalflyers/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends Activity implements ActionBar.TabListener, Dea
         }
     }
 
-    @Override
+    @Overridet 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
